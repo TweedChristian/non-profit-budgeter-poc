c1bfc3277dba5576d380bf351c568fea0069ec56
Christian Tweed <christiantweed@gmail.com>
2023-04-26 20:41:07 -0400

    Added a bunch of icons to the application

---
+++ src/lib/icons/align-right.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  export let className = "stroke-white";
+</script>
+
+<svg
+  width="19"
+  height="19"
+  viewBox="0 0 19 19"
+  class={className}
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <path
+    d="M16.625 4.75H2.375"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+  <path
+    d="M16.625 9.5H7.125"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+  <path
+    d="M16.625 14.25L2.375 14.25"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+</svg>
---
+++ src/lib/icons/calculator.svelte
@@ -0,0 +1,72 @@
+<script lang="ts">
+  export let className = "fill-white";
+</script>
+<svg
+  width="20"
+  height="20"
+  viewBox="0 0 20 20"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+  class={className}
+>
+  <path
+    d="M15 1.66666H5C4.07953 1.66666 3.33333 2.41285 3.33333 3.33332V16.6667C3.33333 17.5871 4.07953 18.3333 5 18.3333H15C15.9205 18.3333 16.6667 17.5871 16.6667 16.6667V3.33332C16.6667 2.41285 15.9205 1.66666 15 1.66666Z"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+  <path
+    d="M6.66667 5H13.3333"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+  <path
+    d="M13.3333 11.6667V15"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+  <path
+    d="M13.3333 8.33334H13.3412"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+  <path
+    d="M10 8.33334H10.0079"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+  <path
+    d="M6.66667 8.33334H6.67458"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+  <path
+    d="M10 11.6667H10.0079"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+  <path
+    d="M6.66667 11.6667H6.67458"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+  <path
+    d="M10 15H10.0079"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+  <path
+    d="M6.66667 15H6.67458"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+</svg>
