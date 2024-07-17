9c0063a153e0265cf19d1d3b88187bb0539abdce
Christian Tweed <christiantweed@gmail.com>
2024-07-17 17:43:15 -0400

    Finished updating dependencies

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,12 @@
-<script lang="ts">
-  import Sidenav from "$lib/components/sidenav.svelte";
-  import { AppShell } from '@skeletonlabs/skeleton';
-  import "../app.css";
-  import '../theme.postcss';
-  import '@skeletonlabs/skeleton/styles/all.css';
-</script>
 
-<AppShell >
-  <svelte:fragment slot="sidebarLeft"><Sidenav/></svelte:fragment>
+
+<AppShell>
+  <svelte:fragment slot="sidebarLeft"><Sidenav></Sidenav></svelte:fragment>
   <slot></slot>
 </AppShell>
 
+<script>import Sidenav from "$lib/components/sidenav.svelte";
+import { AppShell } from "@skeletonlabs/skeleton";
+import "../app.css";
+import "../theme.postcss";
+import "@skeletonlabs/skeleton/styles/all.css";</script>
