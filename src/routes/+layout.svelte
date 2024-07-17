7f9bf589c397402ff94a295068f3353853d20467
Christian Tweed <christiantweed@gmail.com>
2024-07-17 18:08:42 -0400

    Added a todo to migrate layouts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,7 @@
   import "../theme.postcss";
 </script>
 
+<!-- TODO: Migrate to Custom layouts https://next.skeleton.dev/docs/design/layouts -->
 <AppShell>
   <svelte:fragment slot="sidebarLeft"><Sidenav></Sidenav></svelte:fragment>
   <slot></slot>
