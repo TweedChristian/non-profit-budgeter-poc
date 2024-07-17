5e4f61bfcf6d13c5cbf0f771f373e6bf76d89f3b
Christian Tweed <christiantweed@gmail.com>
2024-07-17 18:01:51 -0400

    Updated to skeleton v2

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,17 @@
 import type { Config } from "tailwindcss";
+import { skeleton } from '@skeletonlabs/tw-plugin';
+import { join } from "path";
 
 export default {
-    content: ["./src/**/*.{html,js,svelte,ts}"],
+    darkMode: 'class',
+    content: ["./src/**/*.{html,js,svelte,ts}",join(require.resolve(
+        '@skeletonlabs/skeleton'),
+        '../**/*.{html,js,svelte,ts}'
+    )],
 
     theme: {
         extend: {}
     },
 
-    plugins: [require("@tailwindcss/typography")]
+    plugins: [require("@tailwindcss/typography"), skeleton]
 } as Config;
